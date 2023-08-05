96a27b822a95edb37c5b936de8055f0de5ed166a
Henok <henokmicrometre@gamil.com>
2023-08-05 14:55:04 +0100

    added express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res, next) {
+  res.render("pages/index")
+})
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
